96ddd2bf3d1907e7bf6a3f07a1ab82364618666c
pratik-suryawanshi <47961451+pratik-suryawanshi@users.noreply.github.com>
2019-05-10 13:47:15 +0530

    Created HotelBookingTest.java
    HotelBookingTest.java is an object class of the HotelBookingPage.java Page class.
    
    Added TestNg Annotations

---
+++ src/main/java/HotelBookingTest.java
@@ -0,0 +1,34 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class HotelBookingTest {
+    WebDriver driver;
+
+    @BeforeTest
+    public void intializeDriver() {
+        System.setProperty("webdriver.chrome.driver", "/Users/dipanjankundu/TestProject/app/chromedriver");
+        driver = new ChromeDriver();
+    }
+
+    @Test
+    public void searchHotel(){
+
+        driver.navigate().to("https://www.cleartrip.com/");
+        driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+
+        HotelBookingPage hotelBookingPage = new HotelBookingPage(driver);
+
+        hotelBookingPage.shouldBeAbleToSearchForHotels();
+    }
+
+    @AfterTest
+    public void closeDriver(){
+        driver.quit();
+    }
+}
